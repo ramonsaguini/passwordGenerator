4275e500607ffb7f8de16d23d6c2ee981ca0a0fc
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2023-12-15 19:31:36 +0000

    changed all code

--- starter/script.js
+++ starter/script.js
@@ -24,10 +24,8 @@ var specialCharacters = [
   '_',
   '.'
 ];
-
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
   'a',
@@ -57,7 +55,6 @@ var lowerCasedCharacters = [
   'y',
   'z'
 ];
-
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
   'A',
@@ -87,32 +84,97 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return arr[Math.floor(Math.random() * arr.length)];
 }
+// Function to prompt user for password options
+function getPassOpt() {
+  let length = parseInt(document.getElementById('length').value);
 
+  //if to check if the length it between 8/128Chart and if is not a number
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert('Please enter a number between 8 and 128.');
+    return;
+  }
+  //get checkeds options
+  const lowercase = document.getElementById('lowercase').checked;
+  const uppercase = document.getElementById('uppercase').checked;
+  const numeric = document.getElementById('numeric').checked;
+  const special = document.getElementById('special').checked;
+
+  //if dont check any box will show an alert
+  if (!lowercase && !uppercase && !numeric && !special) {
+    alert('Please select at least one character set.');
+    return;
+  }
+
+  return {
+    length: length,
+    lowercase: lowercase,
+    uppercase: uppercase,
+    numeric: numeric,
+    special: special
+  };
+}
 // Function to generate password with user input
-function generatePassword() {
+function genPassword() {
+  const concatChart = getPassOpt();
+  let charSet = [];
 
+  if (concatChart.lowercase) {
+    charSet = charSet.concat(lowerCasedCharacters);
+  }
+  if (concatChart.uppercase) {
+    charSet = charSet.concat(upperCasedCharacters);
+  }
+  if (concatChart.numeric) {
+    charSet = charSet.concat(numericCharacters);
+  }
+  if (concatChart.special) {
+    charSet = charSet.concat(specialCharacters);
+  }
+
+  let password = '';
+  for (let i = 0; i < concatChart.length; i++) {
+    password += getRandom(charSet);
+  }
+
+  return password;
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var genBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+genBtn.addEventListener('click', function () {
+  var password = genPassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
-}
+  // Write password to the #password input 
+});
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+
+//! You can store the generatedPassword as a string and concat each character OR
+//! as an array and push each character, then join once you have enough characters
+//! Function to prompt user for password options
+//! Prompt for password length
+//! At least 8 characters, no more than 128 characters
+//! Conditional to check that the number that was entered is in range
+//! Prompts store data as strings, so need to parse into a number
+//! If the user's input is out of range, either return out of the function or call the function again
+//! Confirm which character sets to use
+//! If the user answers false for all, either return out of the function or call the function again
+//! Once they select a character set:
+//! Generate a random character for each selected character set
+//! Either push selected character sets to a mega-array of all selected characters
+//! OR you can keep the arrays separate and generate a random number to select the array and another to select the index
+//! Once character sets are selected, move on to generating random characters
+//! Function for getting a random element from an array
+//! Need a variable to hold the password as it's being generated
+//! Need a variable to hold the index that's being generated
+//! For loop that loops the number of times that matches the length the user chose
+//! Generate a random number
+//! That number is the index for a character in the mega-array
+//! So then, mega-array[generated-index] is the actual character
+//! Add that character to the password
+//! Once we finish the for loop, return the generated password
